e92c49f3093ea6c68b163c0c33817042a414b63b
obedientcat <129712357+obedientcat@users.noreply.github.com>
2025-12-31 17:15:53 +0800

    Delete session.js

--- session.js
+++
@@ -1,9 +0,0 @@
-// Save data
-localStorage.setItem('username', 'JohnDoe');
-
-// Get data on page load
-const username = localStorage.getItem('username');
-if (username) {
-  // Use the saved data
-  console.log(`Welcome back, ${username}`);
-}
